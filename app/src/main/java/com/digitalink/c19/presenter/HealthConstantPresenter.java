84da07f85cca546f59cb39a8b9f75c9929d1c2d1
nyemo <Geoleduc>
2020-03-28 17:49:55 -0400

    health constant create

---
+++ app/src/main/java/com/digitalink/c19/presenter/HealthConstantPresenter.java
@@ -0,0 +1,57 @@
+package com.digitalink.c19.presenter;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.annotations.SerializedName;
+
+public class HealthConstantPresenter {
+    @SerializedName("district_id")
+    public String ID;
+
+    @SerializedName("gender")
+    public String gender;
+
+    @SerializedName("age")
+    public int age;
+
+    @SerializedName("weight")
+    public double weight;
+
+    @SerializedName("phone_number")
+    public String phoneNumber;
+
+    @SerializedName("is_diabetic")
+    public boolean is_diabetic;
+
+    @SerializedName("is_hypertensive")
+    public boolean is_hypertensive;
+
+    @SerializedName("is_asthmatic")
+    public boolean is_asthmatic;
+
+    @SerializedName("is_cardio_ischemic")
+    public boolean is_cardio_ischemic;
+
+    @SerializedName("has_lung_disease")
+    public boolean has_lung_disease;
+
+    @SerializedName("has_kidney_disease")
+    public boolean has_kidney_disease;
+
+    @SerializedName("is_smoker")
+    public boolean is_smoker;
+
+    @SerializedName("is_return_from_travel")
+    public boolean is_return_from_travel;
+
+    @SerializedName("longitude")
+    public double longitude;
+
+    @SerializedName("latitude")
+    public double latitude;
+
+    public JsonObject toJson() {
+        String jsonInString = new Gson().toJson(this);
+        return new Gson().fromJson(jsonInString, JsonObject.class);
+    }
+}
